3124e339cf74e68598316be1add80de768e33026
Dylan <dylanvanwinkle@gmail.com>
2020-02-03 19:37:49 -0800

    HELLO

--- _00.py
+++ _00.py
@@ -19,8 +19,8 @@ def main(startAt):
     set_cursor(OFF)
     set_font('Lat15-TerminusBold32x16')
 
-    if startAt <=  1 : wait_for_button_with_message('Carpet');          m01_red_carpet()
-    if startAt <=  2 : wait_for_button_with_message('Boom');            m02_microphone_boom()
+    # if startAt <=  1 : wait_for_button_with_message('Carpet');          m01_red_carpet()
+    # if startAt <=  2 : wait_for_button_with_message('Boom');            m02_microphone_boom()
     # if startAt <=  3 : wait_for_button_with_message('Costume');         m03_costume()
     # if startAt <=  4 : wait_for_button_with_message('Move Set');        m04_move_the_set()
     # if startAt <=  5 : wait_for_button_with_message('Rigging');         m05_rigging()
